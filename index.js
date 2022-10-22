9b5a47fb7d50229fa80068077e874f352448bd45
Ethan Willetts <willetts.ethan@gmail.com>
2022-10-22 16:18:16 -0400

    commit tree 63e7b17914743afb249bb7ee97dab44851108905

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// Code your solutions in this file
+const writtenCards = [];
+
+function writeCards(names, event) {
+    for (let i = 0; i < names.length; i++) {
+        writtenCards[i] = `Thank you, ${names[i]}, for the wonderful ${event} gift!`;
+    };
+    return writtenCards;
+};
+
+function countDown(number) {
+    while (number >= 0) {
+        console.log(number--);
+    }
+}
